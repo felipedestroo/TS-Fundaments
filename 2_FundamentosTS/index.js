b6d0a48bd8a3ebb75ca12fd8510969865ec07f77
Felipe Destro <fdestrocontact@gmail.com>
2024-07-08 16:08:28 -0300

    Feat: Adicionando o tipo number

---
+++ 2_FundamentosTS/index.js
@@ -0,0 +1,6 @@
+// 1 - conhecendo o number
+var a = 10;
+var b = 3.58973;
+console.log(a);
+console.log(typeof (a));
+console.log(b.toPrecision(2));
---
+++ 2_FundamentosTS/index.ts
@@ -0,0 +1,10 @@
+// 1 - conhecendo o number
+
+const a:number = 10
+
+let b:number = 3.58973
+
+console.log(a)
+console.log(typeof(a))
+
+console.log(b.toPrecision(2))
