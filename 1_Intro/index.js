c38b349ddd156cf62e9ca42301ed8665f0a74af2
Felipe Destro <fdestrocontact@gmail.com>
2024-07-08 15:48:10 -0300

    Feat: Aplicando os desafios estabelecidos

---
+++ 1_Intro/index.js
@@ -0,0 +1,7 @@
+function sum(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+console.log(sum(2, 3));
+var a = 5;
+var b = 3;
+console.log(sum(a, b));
---
+++ 1_Intro/index.ts
@@ -0,0 +1,10 @@
+function sum(firstNumber: number, secondNumber: number) {
+    return firstNumber + secondNumber
+}
+
+console.log(sum(2, 3))
+
+const a = 5
+const b = 3
+
+console.log(sum(a, b))
