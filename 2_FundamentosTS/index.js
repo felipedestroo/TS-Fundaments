14c8d2531f602e91b47ca20047c648a73a7e3724
Felipe Destro <fdestrocontact@gmail.com>
2024-07-08 17:22:11 -0300

    Feat: aplicando o formato de string

--- 2_FundamentosTS/index.js
+++ 2_FundamentosTS/index.js
@@ -1,6 +1,27 @@
 // 1 - conhecendo o number
 var a = 10;
 var b = 3.58973;
+var c = 23;
+function sum(a, c) {
+    return a + c;
+}
+console.log(sum(a, c));
 console.log(a);
 console.log(typeof (a));
 console.log(b.toPrecision(2));
+// 2 - conhecendo a string 
+var firstName = "Felipe ";
+var secondName = "Destro";
+var client = "Raimundo";
+function nome(firstName, secondName) {
+    return firstName + secondName;
+}
+console.log("Ol\u00E1, ".concat(nome(firstName, secondName)));
+console.log(typeof (firstName));
+console.log(secondName.toUpperCase());
+console.log(firstName.toLowerCase() + secondName.toUpperCase());
+function hi(client) {
+    console.log("Ol\u00E1, ".concat(client));
+    console.log("Meu nome \u00E9 ".concat(nome(firstName, secondName)));
+}
+console.log(hi(client));
--- 2_FundamentosTS/index.ts
+++ 2_FundamentosTS/index.ts
@@ -4,7 +4,38 @@ const a:number = 10
 
 let b:number = 3.58973
 
+const c:number = 23
+
+function sum(a:number, c:number) {
+    return a + c
+}
+
+console.log(sum(a, c))
+
 console.log(a)
 console.log(typeof(a))
 
 console.log(b.toPrecision(2))
+
+// 2 - conhecendo a string 
+
+  const firstName: string = "Felipe "
+
+  const secondName: string = "Destro"
+
+  const client: string = "Raimundo"
+
+  function nome(firstName: string, secondName: string) {
+    return firstName + secondName
+  }
+
+  console.log(`Olá, ${nome(firstName, secondName)}`)
+  console.log(typeof(firstName))
+  console.log(secondName.toUpperCase())
+  console.log(firstName.toLowerCase() + secondName.toUpperCase())
+
+  function hi(client: string) {
+    console.log(`Olá, ${client}`)
+    console.log(`Meu nome é ${nome(firstName, secondName)}`)
+  }
+  console.log(hi(client))
