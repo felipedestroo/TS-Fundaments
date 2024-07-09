59e0a551d21f51f9a1589d485d43fc8290e9a919
felipedestroo <fdestrocontact@gmail.com>
2024-07-09 14:03:26 -0300

    Feat: funcao anonima

--- 3_Tipos/index.ts
+++ 3_Tipos/index.ts
@@ -33,3 +33,45 @@ newName.push("Silva")
 console.log(newName)
 
 console.log(newName[2])
+
+// 3 - O tipo Any
+
+// Se atentar a NÃO utilizar o Any
+
+const ary1: any = [1, "Felipe", [1, 2, 3], {names: "Silva"}]
+
+console.log(ary1)
+
+ary1.push("Felipe")
+
+console.log(ary1)
+
+// 4 - Parametros tipados e retornos de função
+
+function soma(a: number, b: number) {
+  return a + b
+}
+
+console.log(soma(10, 4))
+
+function conc(c: string, d: string): string {
+  return c + d 
+}
+
+console.log(conc("Felipe ", "Destro"))
+
+// 5- funcao anonima
+
+setTimeout(function() {
+
+  const sallary: number = 227356753.27625
+
+  
+
+  // console.log(parseFloat(sallary))
+
+  // console.log(sallary)
+
+}, 2000)
+
+
